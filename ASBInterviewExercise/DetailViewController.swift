86bfe10d6a25aabc4d402a24e6f68102914810de
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2022-06-17 16:04:54 +1200

    - created the DetailView in storyboard - created the DetailViewController.swift

---
+++ ASBInterviewExercise/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  ASBInterviewExercise
+//
+//  Created by Ronald Fornis Paglinawan on 17/06/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var debitCreditLabel: UILabel!
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
