0bb2a32ffb6dce853348ceae4184fe22258c94a7
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2022-06-19 22:28:53 +1200

    - added test cases

--- ASBInterviewExerciseTests/ASBInterviewExerciseTests.swift
+++ ASBInterviewExerciseTests/ASBInterviewExerciseTests.swift
@@ -17,17 +17,30 @@ class ASBInterviewExerciseTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testTransaction() {
+        let transaction = Transaction(id: 0, transactionDate: "2022-02-22T22:22:22", summary: "Won from Lotto!", debit: 999999.99, credit: 0)
+        
+        XCTAssertEqual(transaction.debit, 999999.99)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testTransactionViewModel() {
+        let transactionViewModel = TransactionViewModel()
+        
+        XCTAssertEqual(transactionViewModel.urlString, "https://gist.githubusercontent.com/Josh-Ng/500f2716604dc1e8e2a3c6d31ad01830/raw/4d73acaa7caa1167676445c922835554c5572e82/test-data.json")
+    }
+    
+    func testDateToString() {
+        let sampleDateString = "2022-02-22T22:22:22"
+        
+        var tempDate = Date()
+        // convert String to Date format
+        if let convertedDate = String.stringToDate(dateString: sampleDateString) {
+            tempDate = convertedDate
         }
+        // convert Date to String format
+        let stringDate = String.dateToString(date: tempDate)
+        
+        XCTAssertEqual(stringDate, "Feb 22, 2022 10:22:22 PM")
     }
-
 }
